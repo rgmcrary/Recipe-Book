19c62886b424dda0d236f85f9f03fb0c05f50b78
Rich McRary <rgmcrary@gmail.com>
2018-02-11 15:13:21 -0500

    Building out component functionality

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,14 +8,22 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a sample.', 'https://www.maangchi.com/wp-content/uploads/2013/09/kimbap.jpg')
+    new Recipe('A Test Recipe', 'This is a sample.', 'https://www.maangchi.com/wp-content/uploads/2013/09/kimbap.jpg'),
+    new Recipe('Another Test Recipe', 'This is a sample.', 'https://www.maangchi.com/wp-content/uploads/2013/09/kimbap.jpg')
+
   ];
 
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
