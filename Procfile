f4e919afb7452bd72d1a6c3b280f7b500d91c43a
Rich McRary <rgmcrary@gmail.com>
2018-04-25 20:09:13 -0400

    Updated server paths

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: npm install
-web: npm start
+web: node server
--- server.js
+++ server.js
@@ -4,32 +4,35 @@ require('zone.js/dist/zone-node');
 require('reflect-metadata');
 
 const express = require('express');
-const ngUniversal = require('@nguniversal/express-engine');
-const { provideModuleMap } = require('@nguniversal/module-map-ngfactory-loader');
-
-const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist-server/main.bundle');
-
-function angularRouter(req, res) {
-  res.render('index', { req, res });
-}
+// const ngUniversal = require('@nguniversal/express-engine');
+// const { provideModuleMap } = require('@nguniversal/module-map-ngfactory-loader');
+//
+// const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist-server/main.bundle');
+//
+// function angularRouter(req, res) {
+//   res.render('index', { req, res });
+// }
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.engine('html', ngUniversal.ngExpressEngine({
-  bootstrap: AppServerModuleNgFactory,
-  providers: [
-    provideModuleMap(LAZY_MODULE_MAP)
-  ]
-}));
-app.set('view engine', 'html');
-app.set('views', 'dist');
+// app.engine('html', ngUniversal.ngExpressEngine({
+//   bootstrap: AppServerModuleNgFactory,
+//   providers: [
+//     provideModuleMap(LAZY_MODULE_MAP)
+//   ]
+// }));
+// app.set('view engine', 'html');
+// app.set('views', 'dist');
 
-app.get('/', angularRouter);
+// app.get('/', angularRouter);
 
 app.use(express.static(`${__dirname}/dist`));
 
-app.get('*', angularRouter);
+// app.get('*', angularRouter);
+app.get('*', (req, res) => {
+  res.sendFile(`${__dirname}/dist/index.html`);
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
